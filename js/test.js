beb2d07150b96ea61887c2c992c8df4a09ecdc44
hana825 <hana825@naver.com>
2022-05-19 02:44:47 +0900

    내용물 삽입, 지역버튼클릭효과

--- js/test.js
+++ js/test.js
@@ -14,3 +14,15 @@ seoul.addEventListener("click", () => {
 //   document.getElementById("incheon_view").style.visibility = "visible";
 //   document.getElementById("seoul_view").style.visibility = "hidden";
 // }
+
+// 인기 관광지 - 지역 버튼 눌렀을 때 효과 주기
+var menuLinks = document.querySelectorAll(".menu-link");
+function clickMenuHandler() {
+  for (var i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].classList.remove("menu-active");
+  }
+  this.classList.add("menu-active");
+}
+for (var i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener("click", clickMenuHandler);
+}
